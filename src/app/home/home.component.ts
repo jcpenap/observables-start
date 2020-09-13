9007892954f16f6928d185c0f645527b1b1fd5c2
Juan Carlos Peña Peña <penapena@uninorte.edu.co>
2020-09-13 16:57:27 -0500

    Observables with error and completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,12 +17,23 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Custom is greater than 3'));
+        }
         count++;
       }, 1000);
     });
 
     this.subcription = customIntervalObservable.subscribe(data => {
       console.log(data);
+    }, (error) => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
     });
   }
 
