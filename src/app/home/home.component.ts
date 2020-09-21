be870c157a06fe437c16ece7ee34a0b499261825
Juan Carlos Peña Peña <ing.juanca@gmail.com>
2020-09-21 08:33:41 -0500

    Using Operator filter and map

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   private subcription: Subscription;
 
-  constructor() { }  
+  constructor() { }
 
   ngOnInit() {
     const customIntervalObservable = Observable.create((observer) => {
@@ -27,7 +28,14 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.subcription = customIntervalObservable.subscribe(data => {
+    this.subcription = customIntervalObservable.pipe(
+      filter((data:number) => {
+        return data > 1;
+      }),
+      map((data: number) => {
+        return 'Round: ' + (data + 1);
+      }))
+    .subscribe(data => {
       console.log(data);
     }, (error) => {
       console.log(error);
